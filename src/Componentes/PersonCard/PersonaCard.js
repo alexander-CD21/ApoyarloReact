8c0d2e496e1f46f29dd43df32f135bc12f39f035
alexander-CD21 <alexander.guillen.v@uni.pe>
2022-03-17 13:04:27 -0500

    practice terminado

---
+++ src/Componentes/PersonCard/PersonaCard.js
@@ -0,0 +1,21 @@
+
+
+const PersonCard = ( props ) => {
+    
+    
+    return(
+        <div>
+            <h1>
+                {props.firstName},{props.lastName}
+            </h1>
+            <h2>
+            Age :{props.age}
+            </h2>
+            <h2>
+            Hair Color: {props.hairColor}
+            </h2>
+        </div>
+    );
+}
+
+export default PersonCard;
