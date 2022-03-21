d66bcc4c30e23958e265a17cfc8748abbb5adbd4
alexander-CD21 <alexander.guillen.v@uni.pe>
2022-03-21 17:58:19 -0500

    core poniendo todo junto

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
+import React from 'react';
 import './App.css';
 import PersonCard from './Componentes/PersonCard/PersonaCard';
 
-function App() {
-  return (
-    <div className="App">
-      <PersonCard firstName="Doe" lastName="Jane" age="45" hairColor="Black"/>
-      <PersonCard firstName="Smith" lastName="John" age="88" hairColor="Brown"/>
-      <PersonCard firstName="Fillmore" lastName="Millard" age="50" hairColor="Brown"/>
-      <PersonCard firstName="Smith" lastName="Maria" age="62" hairColor="Brown"/>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor( props ) {
+    super( props );
+    this.state = {
+        nombre : [{
+            firstName : "Doe",
+            lasrName : "Jane",
+            age :45,
+            hairColor : "Black" 
+        },
+        {
+            firstName : "Smith",
+            lasrName : "John",
+            age :88,
+            hairColor : "Brown"   
+        }]
+    };
 
+  }
+  actualizarEdad1 = () =>{
+    let actualizar = this.state.nombre;
+    actualizar[0].age = actualizar[0].age + 1;
+    this.setState( {nombre : actualizar});
+          
+    
+  } 
+
+  actualizarEdad2 = () => {
+    let actualizar1 = this.state.nombre;
+    actualizar1[1].age = actualizar1[1].age+1;
+    this.setState( { nombre : actualizar1});
+  }
+
+  render(){
+    return (
+      <div>
+        <PersonCard nombre={this.state.nombre} actualizar={this.actualizarEdad1}  actualizar1={this.actualizarEdad2} />
+
+      </div>
+    );
+
+  }
+}
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Componentes/PersonCard/PersonaCard.js
+++ src/Componentes/PersonCard/PersonaCard.js
@@ -1,7 +1,37 @@
+function PersonaCard( props ) {
+    
+     return( 
+        <div>
+           <h1>{props.nombre[0].firstName},{props.nombre[0].lastName}</h1>
+            <h3>Age: {props.nombre[0].age}</h3>
+            <h3>hair Color : {props.nombre[0].hairColor} </h3>
+            <button  onClick={() => props.actualizar()} >
+                Birthday Button for Jane Doe
+            </button>
+            
+           <h1>{props.nombre[1].firstName},{props.nombre[1].lastName}</h1>
+            <h3>Age: {props.nombre[1].age}</h3>
+            <h3>hair Color : {props.nombre[1].hairColor} </h3>
+            <button  onClick={() => props.actualizar1() } >
+                Birthday Button for John Smith
+            </button>
+        </div>
+    )
+}
+
+export default PersonaCard;
 
 
+
+
+
+
+{/*
 const PersonCard = ( props ) => {
     
+    let aumentar = (props) => {
+        props.age = props.age + 1 ;
+      }
     
     return(
         <div>
@@ -14,8 +44,12 @@ const PersonCard = ( props ) => {
             <h2>
             Hair Color: {props.hairColor}
             </h2>
+            <div>
+                <button onClick={this.aumentar} > 
+                    Birthday Button for {props.firstName} {props.lastName}
+                </button>
+            </div>
         </div>
     );
 }
-
-export default PersonCard;
+*/}
